be18ef707f1b2f0f2360d8aa644e709dff903384
elfzweik <wzxuan@foxmail.com>
2022-03-09 17:57:55 -0500

    csrf update

--- blogsite/settings/dev.py
+++ blogsite/settings/dev.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-CSRF_TRUSTED_ORIGINS = ['https://blog.petal2collect.art','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://blog.petal2collect.art','https://*.127.0.0.1','https://www.petal2collect.tk']
 
 try:
     from .local import *
--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = ['https://blog.petal2collect.art','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://blog.petal2collect.art','https://*.127.0.0.1','https://www.petal2collect.tk']
 
 try:
     from .local import *
